98c110c91eea610d39196d16551b12cedb7fd6f6
Jhonny Li <jhonny@jhonny.se>
2019-11-25 03:32:39 +0100

    Return to previous commit

--- JhonnySe/Repositorys/SecretsRepository.cs
+++ JhonnySe/Repositorys/SecretsRepository.cs
@@ -1,73 +1,32 @@
 ﻿using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace JhonnySe.Repositorys
 {
     public class SecretsRepository : ISecretsRepository
     {
-
-        private readonly ILogger _logger;
         private readonly SecretClient _secretClient;
-        private readonly IKeyVaultClient _liveClient;
-        private readonly IHostEnvironment _env;
 
-        public SecretsRepository(ILogger<ISecretsRepository> logger, IKeyVaultClient client, IHostEnvironment env)
+        public SecretsRepository()
         {
-            _env = env;
-            _logger = logger;
-            _logger.LogInformation("Initializing key vault");
-            if (_env.IsProduction())
-            {
-                _liveClient = client;
-            }
-            else
-            {
-                try
-                {
-                    var uri = InitializeKeyVault();
-                    _secretClient = new SecretClient(uri, new DefaultAzureCredential());
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogDebug(ex, "Coult not connect to key-vault");
-                    _logger = null;
-                    _secretClient = null;
-                }
-            }
+            var uri = InitializeKeyVault();
+            _secretClient = new SecretClient(uri, new DefaultAzureCredential());
         }
 
         public async Task<string> GetSecretAsync(string keyName)
         {
-            if (_env.IsProduction())
-            {
-                var secret = await _liveClient.GetSecretAsync(keyName);
-                return secret.Value.ToString();
-            }
-            else
-            {
-                var secret = await _secretClient.GetSecretAsync(keyName);
-                return secret.Value.ToString();
-            }
+            var secret = await _secretClient.GetSecretAsync(keyName).ConfigureAwait(false) ?? throw new ArgumentNullException();
+            return secret.Value.ToString();
         }
 
         public string GetSecret(string keyName)
         {
-            if (_env.IsProduction())
-            {
-                var secret = _liveClient.GetSecretAsync(keyName).Result;
-                return secret.Value.ToString();
-            }
-            else
-            {
-                var secret = _secretClient.GetSecret(keyName);
-                return secret.Value.ToString();
-            }
+            var secret = _secretClient.GetSecret(keyName) ?? throw new ArgumentNullException();
+            return secret.Value.Value.ToString();
         }
 
         private Uri InitializeKeyVault()
