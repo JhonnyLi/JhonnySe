1b765a2cdeb314e1bdf2dfa46eee93c3e4c0ac3d
Jhonny Li <jhonny@jhonny.se>
2019-12-14 15:58:09 +0100

    Updates

--- JhonnySe/WwwRoot/Scripts/app.js
+++ JhonnySe/WwwRoot/Scripts/app.js
@@ -3,5 +3,9 @@
         $scope.model = {};
         $http.get('home/getmodel').then(function (response) {
             $scope.model = response.data;
+            response.data.repositorys.forEach(r => {
+                r.createdDate = new Date(r.createdDate).toLocaleDateString('sv-SE');
+                r.updatedAt = new Date(r.updatedAt).toLocaleDateString('sv-SE');
+            });
         });
     }]);
