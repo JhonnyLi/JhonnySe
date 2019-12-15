25f82eb33d0eeb0eae1075067db897b083abf488
Jhonny Li <jhonny@jhonny.se>
2019-12-15 04:35:23 +0100

    refactoring

--- JhonnySe/WwwRoot/Scripts/app.js
+++ JhonnySe/WwwRoot/Scripts/app.js
@@ -3,7 +3,7 @@
         $scope.model = {};
         $http.get("home/getmodel").then(function (response) {
             $scope.model = response.data;
-            response.data.repositorys.forEach(r => {
+            response.data.repositorys.forEach((r) => {
                 r.createdDate = new Date(r.createdDate).toLocaleDateString("sv-SE");
                 r.updatedAt = new Date(r.updatedAt).toLocaleDateString("sv-SE");
             });
